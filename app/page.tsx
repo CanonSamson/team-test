0a31208e21eaf4e576c3dec7b9767edce306df75
CanonSamson <samsoncanon2018@gmaail.com>
2024-09-12 15:48:23 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,12 @@
-"use client";
-import { ChangeEvent, FormEvent, useState } from "react";
-import { db } from "../firebase";
-import { questions } from "@/content/questions";
-import { collection, doc, setDoc } from "firebase/firestore";
+import dynamic from 'next/dynamic';
 
-// types/models.ts
-export interface Question {
-  question: string;
-  options: string[];
-}
+const MyComponent = dynamic(() => import('@/components/Main'), { ssr: false });
 
-export interface Answers {
-  [key: number]: string;
-}
+
 
 export default function Home() {
-  const [answers, setAnswers] = useState<Answers>({});
-  const [userName, setUserName] = useState("");
-  const [loading, setLoading] = useState(false);
-  const handleChange = (e: ChangeEvent<HTMLInputElement>, index: number) => {
-    setAnswers({ ...answers, [index]: e.target.value });
-  };
-
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    try {
-      setLoading(true);
-      const docRef = doc(db, "answers", userName);
-      await setDoc(docRef, answers);
-      alert("Your answers have been submitted!");
-    } catch (error) {
-      console.error("Error submitting answers:", error);
-      alert("There was an error submitting your answers.");
-    } finally {
-      setLoading(false);
-    }
-  };
 
   return (
-    <div className="container mx-auto p-4">
-      <main className=" flex-col flex items-center justify-center">
-        <div>
-          <h1 className="text-3xl font-bold">Team Question Game</h1>
-          <p>Get to Know you will Love Working with</p>
-          {questions.length}
-        </div>
-
-        <form onSubmit={handleSubmit} className="space-y-4  p-4">
-          <div className=" grid">
-            <label className="text-lg font-semibold">User Name</label>
-            <input
-              placeholder="User Name"
-              className=" h-[45px] border rounded-lg bg-white p-2 "
-              onChange={(e) => {
-                setUserName(e.target.value);
-              }}
-              value={userName}
-            />
-          </div>
-          {questions.map((question, index) => (
-            <div
-              key={index}
-              className="flex border rounded-xl p-4  bg-white shadow-lg flex-col"
-            >
-              <label className="text-lg font-semibold">
-                {question.question}
-              </label>
-              <div className="space-y-2">
-                {question.options.map((option, i) => (
-                  <div key={i} className="flex items-center">
-                    <input
-                      type="radio"
-                      id={`q${index}_o${i}`}
-                      name={`q${index}`}
-                      value={option}
-                      onChange={(e) => handleChange(e, index)}
-                      className="mr-2"
-                      required
-                    />
-                    <label htmlFor={`q${index}_o${i}`}>{option}</label>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
-          <button type="submit" className="bg-blue-500 text-white p-2 rounded">
-            Submit
-          </button>
-        </form>
-      </main>
-    </div>
+    <MyComponent />
   );
 }
--- components/Result.tsx
+++ components/Result.tsx
@@ -1,22 +1,22 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+// // import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const Result = () => {
-  const KEY = "AIzaSyBijzbIDro8hpQHSfxC5BWSCS8Fl4Qn3So";
+// const Result = () => {
+//   // const KEY = "AIzaSyBijzbIDro8hpQHSfxC5BWSCS8Fl4Qn3So";
 
-  const genAI = new GoogleGenerativeAI(KEY);
+//   // const genAI = new GoogleGenerativeAI(KEY);
 
-  const getResponseForGivenPrompt = async () => {
-    try {
-      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-      const result = await model.generateContent(`inputValue`);
-      const response =  result.response;
-      const text =  response.text();
-    } catch (error) {
-      console.log("Something Went Wrong");
-    }
-  };
+//   // const getResponseForGivenPrompt = async () => {
+//   //   try {
+//   //     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+//   //     const result = await model.generateContent(`inputValue`);
+//   //     const response =  result.response;
+//   //     const text =  response.text();
+//   //   } catch (error) {
+//   //     console.log("Something Went Wrong");
+//   //   }
+//   // };
 
-  return <div></div>;
-};
+//   return <div></div>;
+// };
 
-export default Result;
+// export default Result;
